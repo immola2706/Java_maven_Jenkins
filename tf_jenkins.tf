5b590bc3b4e623516ffd4dcab0a4fe7937b2caed
immola2706 <54349107+immola2706@users.noreply.github.com>
2019-08-29 17:55:05 +0530

    updated AMI

--- tf_jenkins.tf
+++ tf_jenkins.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "AppServer-SG" {
 # Creating EC2 Instance to run Tomcat Server
 
 resource "aws_instance" "Tomcat_AppServer" {
-    ami = "ami-07d0cf3af28718ef8"   
+    ami = "ami-0961e3561ddfa2ee3"   
     instance_type = "${var.instance_type}"
     subnet_id = "${aws_subnet.AppServer_subnet.id}"
     vpc_security_group_ids = ["${aws_security_group.AppServer-SG.id}"]
